7ce0976b0ba1a6a8c6e8ff5534406ca7d1f6a231
Leonam Quintão <leonam.quintao@bairesdev.com>
2021-07-20 12:01:02 -0300

    add users migrations

--- database/migrations/20210720142659_users.js
+++ database/migrations/20210720142659_users.js
@@ -1,8 +1,17 @@
+const usersTableName = 'users';
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable(usersTableName, table => {
+    table.string('id', 16).notNullable().primary();
+    table.string('full_name').notNullableable();
+		table.string('email').unique().notNullable();
+		table.string('password').notNullable();
+		table.string('token').defaultTo(null);
+		table.timestamp('created_at').defaultTo(knex.fn.now());
+		table.timestamp('updated_at').defaultTo(null);
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable(usersTableName);
 };
