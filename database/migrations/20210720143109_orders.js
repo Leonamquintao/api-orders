da12cd5da112299b484d144fc267e0837a8221fe
Leonam Quintão <leonam.quintao@bairesdev.com>
2021-07-21 10:19:58 -0300

    add orders migration

--- database/migrations/20210720143109_orders.js
+++ database/migrations/20210720143109_orders.js
@@ -1,8 +1,15 @@
+const ordersTableName = 'orders';
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable(ordersTableName, table => {
+    table.string('id', 16).notNullable().primary();
+    table.string('user_id').notNullableable().references('users.id');
+    table.string('status').notNullableable();
+		table.timestamp('created_at').defaultTo(knex.fn.now());
+		table.timestamp('updated_at').defaultTo(null);
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable(ordersTableName);
 };
