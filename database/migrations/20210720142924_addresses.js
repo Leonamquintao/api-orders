c9cad5f05a49aecfce7a93a7eb6e9d94b45d4076
Leonam Quintão <leonam.quintao@bairesdev.com>
2021-07-21 10:17:07 -0300

    add addresses migration

--- database/migrations/20210720142924_addresses.js
+++ database/migrations/20210720142924_addresses.js
@@ -1,8 +1,17 @@
+const addressesTableName = 'addresses';
 
 exports.up = function(knex) {
+
+  return knex.schema.createTable(addressesTableName, table => {
+    table.string('id', 16).notNullable().primary();
+    table.string('user_id').notNullableable().references('users.id');
+    table.string('address').notNullableable();
+		table.timestamp('created_at').defaultTo(knex.fn.now());
+		table.timestamp('updated_at').defaultTo(null);
+  })
   
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable(addressesTableName);
 };
